be7f8ffc6e6184fa741838db3defa069314c93dc
Panhao Wu <wupanhao@qq.com>
2019-10-28 13:03:52 +0000

    WiFi网页配网

--- router/wifi.js
+++ router/wifi.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const Wifi = require('rpi-wifi-connection');
 const bodyParser = require('body-parser')
+const fs = require('fs');
+const path = require('path');
 
 // create application/json parser
 const jsonParser = bodyParser.json()
@@ -11,6 +13,15 @@ const router = express.Router();
 
 // wpa_cli list_networks -i wlan0
 // wpa_cli remove_network -i wlan0 0
+
+router.use('/', express.static(path.join(__dirname, '../wifi')))
+
+router.get('/', (req, res) => {
+	var form = fs.readFileSync(path.join(__dirname, '../wifi/index.html'), {
+		encoding: 'utf8'
+	});
+	res.send(form);
+})
 
 router.get('/info', function(req, res) {
 	wifi.getStatus().then(info => res.json(info))
@@ -37,6 +48,11 @@ router.post('/connect', jsonParser, function(req, res) {
 				status_code: 0
 			})
 		});
+	} else {
+		res.json({
+			status: 'error',
+			status_code: 101
+		})
 	}
 })
 module.exports = router;
