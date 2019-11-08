a23f7c7ec9a851122b2aa36e8e2ce9eff7fb1dee
Panhao Wu <wupanhao@qq.com>
2019-11-08 03:19:09 +0000

    fix error when sb3 file name encoding incorrect

--- app/router/file-reader/index.js
+++ app/router/file-reader/index.js
@@ -5,9 +5,14 @@ const router = express.Router();
 const recursive = require("recursive-readdir");
 
 const dir = require('../scratch-runner/index').getProgramDir()
-
+console.log(dir)
 router.get('/', function(req, res) {
 	recursive(dir, function(err, files) {
+                if(err) {
+			console.log(err) 
+			res.jsonp([])
+			return
+		}
 		res.jsonp(files);
 	});
 })
