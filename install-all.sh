d4ac3551232c72e0459ca72dc1ada27539cafa39
Panhao Wu <wupanhao@qq.com>
2020-01-16 04:34:45 +0000

    add detection so as to avoid reinstall

--- install-all.sh
+++ install-all.sh
@@ -9,19 +9,29 @@ git clone https://github.com/wupanhao/lepi_gui
 git clone https://github.com/wupanhao/lepi-ros-server
 
 # Install LCD Driver
-cd ~/workspace/lepi_gui
-bash install-lcd.sh
-
+cat /boot/config.txt | grep tft9341
+if [ $? -ne 0 ]; then
+  echo "LCD Driver not install, install now"
+  cd ~/workspace/lepi_gui
+  bash install-lcd.sh
+else
+  echo "LCD Driver installed, ignore"
+fi
 # Install WM8960 Audio Driver
-cd ~/workspace/lepi_gui/conf/WM8960-Audio-HAT
-sudo ./install.sh
+cat /boot/config.txt | grep wm8960
+if [ $? -ne 0 ]; then
+  echo "WM8960 Audio Driver not install, install now"
+  cd ~/workspace/lepi_gui/conf/WM8960-Audio-HAT
+  sudo ./install.sh
 # https://github.com/respeaker/seeed-voicecard
 # https://github.com/waveshare/WM8960-Audio-HAT
-
+else
+  echo "WM8960 Audio Driver installed, ignore"
+fi
 # Install Docker
 docker -v
 if [ $? -ne 0 ]; then
-  echo "Docker not installed, install now~"
+  echo "Docker not install, install now~"
   curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh && rm get-docker.sh
   sudo usermod -aG docker pi
 else
@@ -40,14 +50,14 @@ sudo docker run --rm -t -v /home/pi:/home/pi wupanhao/lepi_server:melodic bash -
 node -v
 if [ $? -ne 0 ]; then
   echo "nodejs not install, install now"
-  cd cd ~/workspace/lepi_gui
+  cd ~/workspace/lepi_gui
   bash install-nodejs.sh
 else
   echo "nodejs installed, ignore"
 fi
 # Install GUI
-source ~/nodejs.sh
 cd ~/workspace/lepi_gui
+source ~/nodejs.sh
 yarn install
 yarn build
 cd app
